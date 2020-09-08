63926b545eb6a660f3a5c0d9745fd0da5fdbb48d
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-08 18:09:30 +0500

    Delete

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-require("./db/mongoose");
-
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-  console.log("Todo Server is up on port " + port);
-});
