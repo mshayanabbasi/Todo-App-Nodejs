4bcd11420a6445bf514712435f2ca4cc5ae7f7fd
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-08 20:26:35 +0500

    Changes

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
 const app = express();
+const todo = require("./api/routes/todo");
+const bodyParser = require("body-parser");
+require("./api/db/mongoose");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/todo", todo);
 
 module.exports = app;
