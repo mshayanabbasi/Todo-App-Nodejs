92b9e7656f679e96d32627f312d9f0f20451f0e0
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-08 16:48:10 +0500

    deleted

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-require("./db/mongoose");
-
-const app = express();
-const port = 5000;
-
-app.listen(port, () => {
-  console.log("Todo Server is up on port " + port);
-});
