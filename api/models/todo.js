2c344356733fe53664da1c53fd8ef024d7a3dd97
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-08 16:44:54 +0500

    Todo Schema

---
+++ api/models/todo.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const todoSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
