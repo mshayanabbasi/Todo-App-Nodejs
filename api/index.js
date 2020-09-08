d2932f10525460ce84e04eb5b3400d10bef870e7
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-09-08 16:45:38 +0500

    Add port

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log("Todo Server is up on port " + port);
